f1f55f1c2c47df802013afb638110988b03a8fc3
nathanKith <ntolparov@gmail.com>
2021-01-10 19:38:38 +0300

    chto sluchilos

--- init.sql
+++ init.sql
@@ -1,23 +1,23 @@
-ALTER SYSTEM SET
-    max_connections = '100';
-ALTER SYSTEM SET
-    shared_buffers = '256MB';
-ALTER SYSTEM SET
-    effective_cache_size = '768MB';
-ALTER SYSTEM SET
-    maintenance_work_mem = '64MB';
-ALTER SYSTEM SET
-    checkpoint_completion_target = '0.7';
-ALTER SYSTEM SET
-    wal_buffers = '7864kB';
-ALTER SYSTEM SET
-    default_statistics_target = '100';
-ALTER SYSTEM SET
-    random_page_cost = '1.1';
-ALTER SYSTEM SET
-    effective_io_concurrency = '200';
-ALTER SYSTEM SET
-    work_mem = '2621kB';
+-- ALTER SYSTEM SET
+--     max_connections = '100';
+-- ALTER SYSTEM SET
+--     shared_buffers = '256MB';
+-- ALTER SYSTEM SET
+--     effective_cache_size = '768MB';
+-- ALTER SYSTEM SET
+--     maintenance_work_mem = '64MB';
+-- ALTER SYSTEM SET
+--     checkpoint_completion_target = '0.7';
+-- ALTER SYSTEM SET
+--     wal_buffers = '7864kB';
+-- ALTER SYSTEM SET
+--     default_statistics_target = '100';
+-- ALTER SYSTEM SET
+--     random_page_cost = '1.1';
+-- ALTER SYSTEM SET
+--     effective_io_concurrency = '200';
+-- ALTER SYSTEM SET
+--     work_mem = '2621kB';
 
 
 CREATE EXTENSION IF NOT EXISTS citext;
